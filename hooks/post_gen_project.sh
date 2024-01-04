2d3085759978f105ffc1f4237e97f2b54830ac16
tomek_juszczyszyn <tomek.juszczyszyn@gmail.com>
2024-01-04 17:20:32 +0100

    added zsh

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+git init
+git add -A
+git commit -m "Initial commit"
+git branch -M main
--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.repo_name }}/config.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.repo_name }}/config.py
@@ -4,16 +4,8 @@ from pathlib import Path
 
 from pydantic import BaseSettings
 
-
-class SystemSettings(BaseSettings):
-    GPU_DEVICE: str = "cuda" if platform.system() == "Linux" else "mps"
-
-
-system_settings = SystemSettings()
-
-
 class Paths(BaseSettings):
-    BASE = Path(os.getcwd())
+    BASE: Path = Path(__file__).parent.parent.parent
     DATA = BASE / "data"
 
 paths = Paths()
